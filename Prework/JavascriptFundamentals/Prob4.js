64cb2b80e25d291b19d9d1636912132508c3b6f5
courjimen <coureyfrance@gmail.com>
2025-01-10 00:45:38 -0500

    attempted prob4

--- Prework/JavascriptFundamentals/Prob4.js
+++ Prework/JavascriptFundamentals/Prob4.js
@@ -1,15 +1,14 @@
-// before the call
-let menu = {
-    width: 200,
-    height: 300,
-    title: "My menu"
-  };
-  
-  multiplyNumeric(menu);
-  
-  // after the call
-  menu = {
+ let menu = {
     width: 400,
     height: 600,
     title: "My menu"
   };
+
+  function multiplyNumeric(num){
+    for (let i=0; i<num.length; i++) {
+      if (typeof num[i] === "number") {
+        console.log(menu[key] *= 2);
+  } 
+}
+  }
+  multiplyNumeric(menu);
