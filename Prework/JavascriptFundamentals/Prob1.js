ee5afaef69ab50614cd1a9127109b33ac1fe746a
courjimen <coureyfrance@gmail.com>
2025-01-09 23:59:56 -0500

    Adding probs 1-4

--- Prework/JavascriptFundamentals/Prob1.js
+++ Prework/JavascriptFundamentals/Prob1.js
@@ -1,7 +1,14 @@
 // Write the code, one line for each action:
 
 // Create an empty object user.
+let user = {};
 // Add the property name with the value John.
+user.propName = "John";
 // Add the property surname with the value Smith.
+user.surname = "Smith";
 // Change the value of the name to Pete.
+user.propName = "Pete"
+console.log(user);
 // Remove the property name from the object.
+delete user.propName
+console.log(user);
