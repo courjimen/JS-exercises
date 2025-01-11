63b0971e69c4178aea59cfde1b89dd1c2af5c73c
courjimen <coureyfrance@gmail.com>
2025-01-10 23:54:56 -0500

    JavaFun #4

--- Prework/JavascriptFundamentals/Prob4.js
+++ Prework/JavascriptFundamentals/Prob4.js
@@ -1,14 +1,15 @@
- let menu = {
-    width: 400,
-    height: 600,
-    title: "My menu"
-  };
+// let menu = {
+//   width: 400,
+//   height: 600,
+//   title: "My menu"
+// };
 
-  function multiplyNumeric(num){
-    for (let i=0; i<num.length; i++) {
-      if (typeof num[i] === "number") {
-        console.log(menu[key] *= 2);
-  } 
-}
+multiplyNumeric(menu);
+
+function multiplyNumeric(menu){
+    for(let key in menu){
+      if(typeof menu[key]=== 'number') {
+        menu[key] *= 2;
+      }
+    }
   }
-  multiplyNumeric(menu);
