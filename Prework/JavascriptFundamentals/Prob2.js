662cfda778b31cc47dc071b409afda07df0280ac
courjimen <coureyfrance@gmail.com>
2025-01-10 23:47:54 -0500

    JavaFun #2

--- Prework/JavascriptFundamentals/Prob2.js
+++ Prework/JavascriptFundamentals/Prob2.js
@@ -1,9 +1,15 @@
 //Write the function isEmpty(obj) which returns true if the object has no properties, false otherwise.
-/*let schedule = {};
 
-alert( isEmpty(schedule) ); // true
+function isEmpty(obj) {
+   for(let key in obj) {
+    return false;
+   } return true;
+}
+
+let schedule = {};
+
+console.log(isEmpty(schedule)); // true
 
 schedule["8:30"] = "get up";
 
-alert( isEmpty(schedule) ); // false
- */
+console.log(isEmpty(schedule)); // false
